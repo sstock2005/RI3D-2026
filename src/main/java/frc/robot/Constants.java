1250ae25e17ccced14c05102c4cfeee40579ba2e
sstock2005 <144393153+sstock2005@users.noreply.github.com>
2026-01-10 19:18:59 -0600

    Add Intake Subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,21 +1,26 @@
 package frc.robot;
 
-import static edu.wpi.first.units.Units.Amps;
-
-import edu.wpi.first.units.measure.Current;
-
-public class Constants 
-{
+public class Constants {
     public static final int kDriverControllerPort = 0;
 
-    public static final String kCanBusName = "rio";
-
-    public static final class DrivetrainConstants
-    {
+    public static final class DrivetrainConstants {
         public static final int kLeftLeaderPort = 2;
         public static final int kLeftFollowerPort = 3;
         public static final int kRightLeaderPort = 4;
         public static final int kRightFollowerPort = 5;
         public static final double kStickDeadband = 0.1;
+    }
+
+    public static final class IntakeConstants {
+        // Port Constants
+        public static final int kIntakeRotationPort = 0;
+        public static final int kIntakeWheelPort = 0;
+        public static final int kIntakeRotationEncoderChannelA = 0;
+        public static final int kIntakeRotationEncoderChannelB = 0;
+
+        // Motor Configuration Constants
+        public static final int kSupplyCurrentLimit = 60;
+        public static final int kIntakeRotationStatorCurrentLimit = 120;
+        public static final int kIntakeRotationWheelStatorCurrentLimit = 120;
     }
 }
